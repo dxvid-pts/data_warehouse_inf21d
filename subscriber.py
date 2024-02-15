135d7360392bafbf39bfea4e910740b097335f8b
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2024-02-15 12:33:23 +0100

    subscriber

--- subscriber.py
+++ subscriber.py
@@ -1,16 +1,18 @@
 import paho.mqtt.client as mqtt
+import json
+
+from db import create_table_if_not_exist, write_row
+
+create_table_if_not_exist()
+
 def on_message(client, userdata, message):
+    print("Received message: " + str(message.payload))
+    # Write the message to the database
+    write_row(json.loads(message.payload))
 
-#<JSON Message in DB-Tabelle staging.messung schreiben>
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "j@L&DFTofpqqZ2Vkuu3VJT6AxmNU3m7dTt&CVdaomPqdjxncbjhRVms62TQh8QPEtpCZpHndC@%T!JS8Q%g$QoQcD&!tCVe$nv@DpidnU8tTjzpKeP&NqkcymX*i8gJH", clean_session=False)
+mqttc.on_message = on_message
+mqttc.connect("broker.hivemq.com", 1883, 60)
+mqttc.subscribe("DataMgmt", qos=1)
 
-from db import create_table_if_not_exist
-
-
-def main():
-    # Your primary program logic goes here
-    print("This is inside the main function")
-    create_table_if_not_exist()
-    print("This is the end of the main function")
-
-if __name__ == "__main__":
-    main()
+mqttc.loop_forever()
